9cabf508e3aaf3b6a918b62631b3443082c5d4e0
Folahanmi Kolawole <emmanuelkolawole4@gmail.com>
2023-03-09 13:13:14 +0100

    first commit

---
+++ ScudApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScudApp
+//
+//  Created by ULU on 09/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
